c04481b0c58dc6657d61c314c778fe3061fbbb27
mockersf <mockersf@gmail.com>
2014-09-07 23:19:04 +0200

    first try at vagrant with a sh provisioner, issue with headless firefox

---
+++ Pallas/vagrant/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y unzip
+apt-get install -y firefox
+apt-get install -y python-pip
+apt-get install -y default-jdk
+apt-get install -y apache2
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
+pip install selenium
+pip install browsermob-proxy
+echo "downloading browsermob proxy"
+wget -nv https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip
+unzip browsermob-proxy-2.0-beta-9-bin.zip
