3530f3faf4079c4b9cc6c269aac332069845d820
mockersf <mockersf@gmail.com>
2014-09-07 23:19:41 +0200

    still issue with headless FF

--- Pallas/vagrant/bootstrap.sh
+++ Pallas/vagrant/bootstrap.sh
@@ -3,9 +3,11 @@
 apt-get update
 apt-get install -y unzip
 apt-get install -y firefox
+apt-get install -y xvfb
 apt-get install -y python-pip
 apt-get install -y default-jdk
 apt-get install -y apache2
+Xvfb :10 -ac
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
@@ -14,3 +16,4 @@ pip install browsermob-proxy
 echo "downloading browsermob proxy"
 wget -nv https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip
 unzip browsermob-proxy-2.0-beta-9-bin.zip
+export DISPLAY=:10
