3dfb5e90284a45cec4004201b43e4587a0ba403e
mockersf <mockersf@gmail.com>
2014-09-08 22:51:21 +0200

    do not unzip file if already exist

--- Pallas/vagrant/puppet/modules/browsermob_proxy/manifests/init.pp
+++ Pallas/vagrant/puppet/modules/browsermob_proxy/manifests/init.pp
@@ -4,13 +4,14 @@ class browsermob_proxy {
   }
   ->
   exec { 'download browsermob proxy':
-    command => '/usr/bin/wget -nv https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip',
+    command => '/usr/bin/wget -nv https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip -O /home/vagrant/browsermob-proxy-2.0-beta-9-bin.zip',
     creates => '/home/vagrant/browsermob-proxy-2.0-beta-9-bin.zip',
   }
   ->
   exec { 'unzip browsermob proxy':
-    command => '/usr/bin/unzip browsermob-proxy-2.0-beta-9-bin.zip',
-    require => Exec['download browsermob proxy']
+    command => '/usr/bin/unzip /home/vagrant/browsermob-proxy-2.0-beta-9-bin.zip',
+    require => Exec['download browsermob proxy'],
+    unless => '/bin/ls /home/vagrant/browsermob-proxy-2.0-beta-9'
   }
   ->
   package { ['browsermob-proxy']:
