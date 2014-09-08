c958fec384ed8c6a54db19c754ea12ec3af5b35e
mockersf <mockersf@gmail.com>
2014-09-08 21:33:15 +0200

    now can take a target url as a parameter

--- Pallas/Main/Configuration.py
+++ Pallas/Main/Configuration.py
@@ -1,6 +1,7 @@
-from singleton import singleton
 from argparse import ArgumentParser
 import logging
+
+from singleton import singleton
 
 
 @singleton
@@ -10,10 +11,12 @@ class Configuration(object):
         cli_parser = ArgumentParser()
         group = cli_parser.add_argument_group()
         group.add_argument("-v", "--log-level", help="Log level", type=str, default="INFO")
+        group.add_argument("-t", "--target", help="Log level", type=str, default="http://github.com/")
         group.add_argument("--proxy-path", help="Path to browsermob proxy executable", type=str, default=None)
         cli_args = cli_parser.parse_args()
 
         self._log_level = cli_args.log_level
+        self._target = cli_args.target
         self._proxy_path = cli_args.proxy_path
 
     @property
@@ -33,7 +36,10 @@ class Configuration(object):
             return logging.CRITICAL
         return logging.FATAL
 
-
     @property
     def proxy_path(self):
         return self._proxy_path
+
+    @property
+    def target(self):
+        return self._target
--- Pallas/Main/pallas.py
+++ Pallas/Main/pallas.py
@@ -1,4 +1,5 @@
 import logging
+from urlparse import urlparse
 
 from Configuration import Configuration
 from Browser import Browser
@@ -6,12 +7,12 @@ from Browser import Browser
 config = Configuration()
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=config.get_log_level())
 
-url = 'github.com'
+url = config.target
 
 browser = Browser()
 browser.start()
 #strange behaviour from browsermob proxy, dsn doesn't always work
-browser.add_remap_urls([url])
-browser.get('http://' + url)
+browser.add_remap_urls([urlparse(url).hostname])
+browser.get(url)
 browser.study_state()
 browser.stop()
