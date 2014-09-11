04b055003af8c4d4bad3d01bb3066cf4e1e8c440
mockersf <mockersf@gmail.com>
2014-09-12 01:24:42 +0200

    latest selenium compatible with latest firefox

--- vagrant/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#temporary issue between firefox and selenium
-wget -nv http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/30.0/linux-i686/en-US/firefox-30.0.tar.bz2
-tar jxf firefox-30.0.tar.bz2
-mv /usr/lib/firefox /usr/lib/firefox_last
-mv firefox /usr/lib/firefox
-cp /usr/lib/firefox_last/firefox.sh /usr/lib/firefox/firefox.sh
