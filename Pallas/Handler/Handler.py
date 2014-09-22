b4df06841b391be3707ff95d4ff15465d5e3754d
mockersf <mockersf@gmail.com>
2014-09-22 23:23:17 +0200

    starting with a page a little more populated

--- Pallas/Handler/Handler.py
+++ Pallas/Handler/Handler.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from wtforms import Form, TextField, validators
+from flask import Flask, render_template, flash, request
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
 
-@app.route("/")
+class TargetSiteForm(Form):
+    target = TextField('target', [validators.Length(min=4, max=250)])
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    form = TargetSiteForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('going to analyse %s' % form.target)
+        #return redirect(url_for('login'))
+    return render_template('index.html', form=form)
+
+    res = '<html>'
+    res += header()
+    res += '<div><form>target : <input type="text" name="target"></form>'
+    res += '</html>'
+    return res
