9cebc2ef3a95e7ff42cd899c25c1e01f818e2f53
mockersf <mockersf@gmail.com>
2014-09-22 21:09:38 +0200

    run unit tests before integration tests

--- test/run_test.sh
+++ test/run_test.sh
@@ -4,6 +4,7 @@ Xvfb :10 -ac &
 pid=$!
 
 export DISPLAY=:10
+py.test --cov-report term-missing --cov /Pallas/Pallas /Pallas/Pallas/tests
 sudo -E python /Pallas/Pallas/pallas.py --log-level DEBUG --proxy-path /home/vagrant/browsermob-proxy-2.0-beta-9/bin/ --target "http://localhost:7337/"
 
 kill $pid
