deeaace0a2d00ed9384463a859bd2cde34bf89d6
mockersf <mockersf@gmail.com>
2014-09-24 22:56:21 +0200

    can now choose browser to do the 'sploring

--- Pallas/Handler/Handler.py
+++ Pallas/Handler/Handler.py
@@ -1,21 +1,15 @@
-from wtforms import Form, TextField, validators
+from wtforms import Form, TextField, SelectField, validators
 from flask import Flask, render_template, flash, request
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
 
 class TargetSiteForm(Form):
     target = TextField('target', [validators.Length(min=4, max=250)])
+    browser = SelectField('browser', choices=[('phantom', 'phantomJS'), ('ff', 'Firefox'), ('chrome', 'Chrome'), ('ie', 'Internet Explorer')])
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     form = TargetSiteForm(request.form)
     if request.method == 'POST' and form.validate():
-        flash('going to analyse %s' % form.target)
-        #return redirect(url_for('login'))
+        flash('going to analyse %s with %s' % (form.target.data , form.browser.data))
     return render_template('index.html', form=form)
-
-    res = '<html>'
-    res += header()
-    res += '<div><form>target : <input type="text" name="target"></form>'
-    res += '</html>'
-    return res
