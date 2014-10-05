ea4415fc035a2bad41ade8882398650b20ac3518
mockersf <mockersf@gmail.com>
2014-10-05 22:41:37 +0200

    update on packages needed

--- vagrant/puppet/modules/python-modules/manifests/init.pp
+++ vagrant/puppet/modules/python-modules/manifests/init.pp
@@ -1,17 +1,12 @@
 class python-modules {
-  package { ['selenium']:
+  package { ['libx32z1-dev', 'libxml2-dev', 'libxslt1-dev', 'python-dev']:
+    ensure => present,
+  }
+  package { ['selenium', 'flask', 'lxml', 'pygexf']:
     ensure => present,
     provider => pip,
   }
-  package { ['pytest']:
-    ensure => present,
-    provider => pip,
-  }
-  package { ['pytest-cov']:
-    ensure => present,
-    provider => pip,
-  }
-  package { ['flask']:
+  package { ['pytest', 'pytest-cov']:
     ensure => present,
     provider => pip,
   }
