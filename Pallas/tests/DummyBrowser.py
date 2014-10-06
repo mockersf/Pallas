41ac03c1bc86c3e8f237f18574f69489dbd51ab3
mockersf <mockersf@gmail.com>
2014-10-06 21:35:16 +0000

    dummy browser

---
+++ Pallas/tests/DummyBrowser.py
@@ -0,0 +1,50 @@
+import random
+import uuid
+from Main.singleton import singleton
+
+class DummyElement(object):
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
+        self.attributes = {}
+
+    def get_attribute(self, attribute):
+        if attribute not in self.attributes:
+            self.attributes[attribute] = str(uuid.uuid4())
+        return self.attributes[attribute]
+
+    def click(self):
+        dummy = DummyBrowser()
+        dummy.click(self.tag_name)
+
+@singleton
+class DummyBrowser(object):
+    def __init__(self, seed):
+        self.actions = []
+        self.entropy = 21
+
+    def get(self, url):
+        self.entropy -= 1
+        self.actions.append({'action': 'get', 'target': url})
+        self.current_url = url
+        self.page_source = 'body:get %s' % url
+
+    def click(self, url):
+        self.entropy -= 1
+        self.actions.append({'action': 'element.click', 'target': url})
+        self.current_url = url
+        self.page_source = 'body:click %s' % url
+
+    def find_elements_by_tag_name(self, tag):
+        returns = random.randint(int(self.entropy/10), int(self.entropy/10) + 3)
+        self.actions.append({'action': 'find_elements_by_tag_name', 'target': tag, 'nb': returns})
+        elems = []
+        for i in xrange(returns):
+            elems.append(DummyElement(tag))
+        return elems
+
+    def find_element_by_xpath(self, xpath):
+        self.actions.append({'action': 'find_element_by_xpath', 'target': xpath})
+        return DummyElement('new_tag_%s' % xpath)
+
+    def quit(self):
+        self.actions.append({'action': 'quit', 'target': None})
---
+++ Pallas/tests/test_main/test_main_browser.py
@@ -0,0 +1,48 @@
+import os.path
+import sys
+import uuid
+import random
+
+def setup_module(module):
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+class Test_Main_Browser(object):
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def test_basic(self):
+        from Main.Configuration import Configuration
+        from Site.Site import Site
+        from Main.Browser import Browser
+        from tests.DummyBrowser import DummyBrowser
+        dummy = DummyBrowser(random.random())
+        config = Configuration(['-b', 'Dummy'])
+        config._browser = 'Dummy'
+        site_url = str(uuid.uuid4())
+        site = Site(site_url)
+        browser = Browser()
+        browser.start(site)
+        browser.get()
+        browser.study_state()
+        assert len(dummy.actions) == 3
+        assert dummy.actions[0] == {'action': 'get', 'target': site_url}
+        assert dummy.actions[1]['action'] == 'find_elements_by_tag_name'
+        assert dummy.actions[1]['target'] == 'input'
+        assert dummy.actions[2]['action'] == 'find_elements_by_tag_name'
+        assert dummy.actions[2]['target'] == 'a'
+        assert len(site._pages) == 1
+        assert len(site._connections) == dummy.actions[2]['nb'] + 1
+        assert len(site._connections) > 1
+        assert len([explored for explored in list(site._connections.values()) if explored['explored']]) == 1
+        actions = site.get_first_connection_unexplored()
+        assert len(actions) == 1
+        actions[0].do()
+        assert len(dummy.actions) == 5
+        assert dummy.actions[3]['action'] == 'find_element_by_xpath'
+        assert dummy.actions[4]['action'] == 'element.click'
+        browser.stop()
+        assert len(dummy.actions) == 6
+        assert dummy.actions[5]['action'] == 'quit'
