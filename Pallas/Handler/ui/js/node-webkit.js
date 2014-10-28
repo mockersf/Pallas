65bb181891176f8ad2ebdaf16a82c755541ef134
mockersf <mockersf@gmail.com>
2014-10-28 22:29:06 +0000

    trying node-webkit

---
+++ Pallas/Handler/ui/js/node-webkit.js
@@ -0,0 +1,7 @@
+//example of how to launch a process
+//var spawn = require('child_process').spawn,
+//grep  = spawn('pwd');
+
+//console.log('Spawned child pid: ' + grep.pid);
+//grep.stdin.end();
+console.log('woohoooo');
